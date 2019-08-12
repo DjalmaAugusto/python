70693960e6a2db5fc28c1692279911b496010506
DjalmaAugusto <djalmaaugusto_@hotmail.com>
2019-08-12 08:41:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, jsonify
+
+HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS', 'localhost')
+APP_NAME = os.environ.get('OPENSHIFT_APP_NAME', 'flask')
+IP = os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1')
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+HOME_DIR = os.environ.get('OPENSHIFT_HOMEDIR', os.getcwd())
 
 app = Flask(__name__)
 
@@ -6,7 +13,14 @@ print(__name__)
 
 @app.route('/')
 def index():
-  return 'Index Page'
+  return jsonify({
+        'host_name': HOST_NAME,
+        'app_name': APP_NAME,
+        'ip': IP,
+        'port': PORT,
+        'home_dir': HOME_DIR,
+        'host': socket.gethostname()
+    })
 
 @app.route('/hello')
 def hello():
@@ -40,4 +54,4 @@ def login():
     return "GET"
 
 if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=PORT)
